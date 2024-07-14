1243df2f2551ec44600a30e250a66bc450547bf9
Burhan Mubarok <burhanmubarok@gmail.com>
2024-07-15 00:29:06 +0700

    Init main body

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,102 @@
-import Image from "next/image";
+import Image from "next/image"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+import { Divider } from '@mui/material';
+
+interface CircleImageProps {
+  src: string;
+  alt: string;
+  size: string;
+}
+
+const CircleImage: React.FC<CircleImageProps> = ({ src, alt, size }) => {
+  return (
+    <div className={`relative overflow-hidden rounded-full`} style={{ width: size, height: size }}>
+      <Image src={src} alt={alt} layout="fill" className="object-cover" />
+    </div>
+  );
+};
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col min-w-screen min-h-screen bg-black text-white">
+      <div className="flex-grow flex justify-center">
+        <div className="w-4/5 flex flex-col p-4">
+          <div className="flex flex-col space-y-4 h-full">
+            <div className="p-4 flex flex-row-reverse items-center gap-4">
+              <div className="p-4 flex flex-col group border border-transparent rounded-lg hover:border-pink-300 space-y-4">
+                <h1 className="text-3xl items-center">Butuh pelatih yang berpengalaman?</h1>
+                <p>
+                  Tingkatkan potensimu dengan pelatih tenis meja kami yang ahli dan berpengalaman.<br />
+                  Baik kamu pemula atau profesional, pelatih kami siap mengasah kemampuanmu.<br />
+                  Bergabunglah sekarang dan rasakan pelatihan berkualitas yang menjamin peningkatan dan kesenangan!
+                </p>
+                <Button className="self-start" variant="outlined" color="secondary">Booking Pelatih</Button>
+              </div>
+              <Image className="rounded-md" width={800} height={400} alt="image" src="/teaching-1.png"></Image>
+            </div>
+            <div className="p-4 flex flex-row items-center gap-4">
+              <div className="p-4 flex flex-col group border border-transparent rounded-lg hover:border-pink-300">
+                <h1 className="text-3xl items-center mb-4">Butuh lawan yang lebih jago untuk sparing?</h1>
+                <p>
+                  Tingkatkan permainanmu dengan bertanding bersama rekan tenis meja kami yang sangat terampil dan berpengalaman.<br />
+                  Cocok untuk semua level, rekan-rekan kami siap memberikan tantangan dan dukungan yang kamu butuhkan untuk berkembang.<br />
+                  Bergabunglah sekarang dan pesan sesi sparing untuk mengasah keterampilanmu ke tingkat yang lebih tinggi!
+                </p>
+                <Button className="self-end" variant="outlined" color="secondary">Booking Teman Sparing</Button>
+              </div>
+              <Image className="rounded-md" width={800} height={400} alt="image" src="/sparing-2.png"></Image>
+            </div>
+            <div className="p-4 flex flex-row-reverse items-center gap-4">
+              <div className="p-4 flex flex-col group border border-transparent rounded-lg hover:border-pink-300 space-y-4">
+                <h1 className="text-2xl items-center">Bagaimana platform pingpox bekerja?</h1>
+                <p>
+                  Temukan kemudahan sistem otomatis kami yang memungkinkan Anda dengan mudah<br />
+                  memesan pelatih, mencari rekan sparring, dan bahkan memesan lapangan/meja.<br />
+                  Hanya dengan beberapa klik, PingPox memastikan Anda<br />
+                  memiliki segala yang dibutuhkan untuk pengalaman tenis meja yang efektif.<br />
+                  Bergabung sekarang dan tingkatkan permainan Anda dengan kemudahan dan efisiensi platform kami!<br />
+                  Dan jangan lupa, kami juga secara berkala menyelenggarakan turnamen dan event menarik lainnya!<br />
+                </p>
+                <div className="flex flex-row items-center justify-center gap-4">
+                  <Button className="self-start" variant="outlined" color="secondary">Explore Lebih Lanjut</Button>
+                  atau
+                  <Button className="self-start" variant="outlined" color="secondary">Pantau Project Pingpox</Button>
+                </div>
+              </div>
+              <Image className="rounded-md" width={800} height={400} alt="image" src="/court-5.png"></Image>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col bg-white text-center text-black">
+        <div className="m-4">
+          <span className="text-5xl">Lokasi Latihan</span>
+        </div>
+        <Divider />
+        <div className="flex flex-row justify-evenly items-center p-4">
+          <div>
+            <CircleImage src="/court-5.png" alt="Circle Image" size="400px" />
+            <br />
+            <a href="https://maps.app.goo.gl/qAicKmJao7NDQ5UZ9" target="_blank" className="p-4 text-xl font-light underline">GOR Oyuda</a>
+          </div>
+          <div>
+            <CircleImage src="/court-3.png" alt="Circle Image" size="400px" />
+            <br />
+            <a href="https://maps.app.goo.gl/6r7ekeYk4gMSR7qt7" target="_blank" className="p-4 text-xl font-light underline">GOR Plara</a>
+          </div>
+          <div>
+            <CircleImage src="/court-4.png" alt="Circle Image" size="400px" />
+            <br />
+            <a href="https://maps.app.goo.gl/UpoeqM2ET6axPvKT8" target="_blank" className="p-4 text-xl font-light underline">GOR Rengganis</a>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </main >
   );
 }
