0cd86c744dad035a8688a98b5b604d4d63c15381
Burhan Mubarok <burhan.mubarok@agoda.com>
2024-07-15 00:27:37 +0700

    Create footer component

---
+++ components/footer.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import Image from "next/image";
+import { PiYoutubeLogo, PiInstagramLogo, PiTiktokLogo } from "react-icons/pi";
+
+const Footer = () => {
+    return (
+        <div id="footer" className="flex flex-row gap-4 p-4 bottom-0 left-0 right-0">
+            <div id="footer-l" className="basis-1/3 p-5 transition-colors group border border-transparent rounded-lg hover:border-pink-300 hover:dark:bg-neutral-800/10 hover:text-pink-300">
+                <Image src='/pingpox-logo.png' alt="Pingpox Home" width={60} height={60} />
+                <pre>pingpox.com</pre>
+                <br />
+                <p className="font-thin text-xs">Adalah platform olahraga tenis meja berbasis teknologi dan beroperasi 24/7, mulai dari penyewaan meja, janjian dengan pelatih dan juga berlangganan kelas latihan dan penyelenggaraan pertandigan/turnamen terotomasi</p>
+            </div>
+            <div id="footer-c" className="basis-1/3 p-5 transition-colors border border-transparent rounded-lg hover:border-pink-300 hover:dark:bg-neutral-800/10 hover:text-pink-300">
+                <div className="flex flex-col space-y-2">
+                    <span className="mb-4 font-semibold">
+                        Link Terkait
+                    </span>
+                    <Link href="/events">
+                        <span className="hover:text-pink-500">Events</span>
+                    </Link>
+                    <Link href="/members">
+                        <span className="hover:text-pink-500">Membership</span>
+                    </Link>
+                    <Link href="/tournaments">
+                        <span className="hover:text-pink-500">Penyelenggaraan Turnamen</span>
+                    </Link>
+                    <Link href="/franchises">
+                        <span className="hover:text-pink-500">Kerjasama Franchise</span>
+                    </Link>
+                    <Link href="/inquiries">
+                        <span className="hover:text-pink-500">Narahubung</span>
+                    </Link>
+                    <Link target="_blank" href="https://github.com/users/burubur/projects/6">
+                        <span className="hover:text-pink-500">Project Pingpox</span>
+                    </Link>
+                </div>
+            </div>
+            <div id="footer-l" className="group basis-1/3 p-5 transition-colors border border-transparent rounded-lg hover:border-pink-300 hover:dark:bg-neutral-800/10 hover:text-pink-300">
+                <div className="flex flex-col space-y-2">
+                    <span className="mb-4 font-semibold">
+                        Media Sosial Kami
+                    </span>
+                    <div className="flex space-x-4">
+                        <a href="#" className="hover:text-pink-500">
+                            <PiYoutubeLogo title="Youtube" size="36px" />
+                        </a>
+                        <a href="#" className="hover:text-pink-500">
+                            <PiInstagramLogo title="Youtube" size="36px" />
+                        </a>
+                        <a href="#" className="hover:text-pink-500">
+                            <PiTiktokLogo title="Youtube" size="36px" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Footer;
