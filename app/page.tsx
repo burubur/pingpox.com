1e60bb0f17d0d1c037fa7bff8a53a86b9f10d9f0
Burhan Mubarok <burhanmubarok@gmail.com>
2024-08-25 22:10:57 +0700

    Modify root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
-import Image from "next/image"
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Button from '@mui/material/Button';
-import { Divider } from '@mui/material';
-import Location from "../components/location"
-import Introduction from "../components/introduction"
+"use client";
 
-const Home = () => {
+import Image from "next/image";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Divider } from "@mui/material";
+import Location from "../components/location";
+import Introduction from "../components/introduction";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { User, getServerSession } from "next-auth";
+import { Button, Typography } from "@mui/material";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const Home = ({ Component, pageProps }: AppProps) => {
+  const { data: session } = useSession();
+  console.log("fetching session data");
+  console.log(session);
+
   return (
-    <main className="flex flex-col min-w-screen bg-black text-white">
-      <Introduction />
-      <Location />
-    </main >
+    <SessionProvider>
+      <div>
+        <Introduction />
+        <Location />
+      </div>
+    </SessionProvider>
   );
-}
+};
 
 export default Home;
