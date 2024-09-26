0853d6cc22e1bb59353aec03d8a5881e8bb2b091
Burhan Mubarok <burhanmubarok@gmail.com>
2024-09-26 22:46:00 +0700

    Refactor UI: working hours

--- app/operations/workinghours/page.tsx
+++ app/operations/workinghours/page.tsx
@@ -6,73 +6,127 @@ import {
   Box,
   Paper,
   Typography,
-  List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Divider,
 } from "@mui/material";
 import {
-  Star as StarIcon,
   Restaurant,
   SportsTennis,
   WifiOff,
   DateRange,
   BeachAccess,
+  Coffee,
 } from "@mui/icons-material";
-import CustomAppBar from "./../schedules/appbar"
+import CustomAppBar from "./../schedules/appbar";
 
 const CustomPaper = styled(Paper)(({ theme }) => ({
-  //   padding: theme.spacing(2),
   ...theme.typography.body2,
   textAlign: "center",
 }));
 
-const Hourly = () => {
+const ByDay = () => {
   return (
     <Box>
-    <CustomAppBar />
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <SportsTennis />
-          </ListItemIcon>
-          <ListItemText primary="08:00 - 12:00" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <Restaurant />
-          </ListItemIcon>
-          <ListItemText primary="12:00 - 13:00" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <SportsTennis />
-          </ListItemIcon>
-          <ListItemText primary="13:00 - 16:00" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <Restaurant />
-          </ListItemIcon>
-          <ListItemText primary="16:00 - 17:00" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <SportsTennis />
-          </ListItemIcon>
-          <ListItemText primary="19:00 - 20:00" />
-        </ListItemButton>
-      </ListItem>
+      <Typography variant="subtitle1" align="left">
+        Days
+      </Typography>
+      <CustomPaper variant="elevation" elevation={3}>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DateRange />
+            </ListItemIcon>
+            <ListItemText primary="Saturday - Sunday" />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DateRange />
+            </ListItemIcon>
+            <ListItemText primary="Monday, Wednesday, Thursday" />
+          </ListItemButton>
+        </ListItem>
+      </CustomPaper>
     </Box>
+  );
+};
+
+const ByHour = () => {
+  return (
+    <Box>
+      <CustomPaper variant="elevation" elevation={3}>
+        <CustomAppBar />
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SportsTennis />
+            </ListItemIcon>
+            <ListItemText primary="08:00 - 12:00" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Restaurant />
+            </ListItemIcon>
+            <ListItemText primary="12:00 - 13:00" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SportsTennis />
+            </ListItemIcon>
+            <ListItemText primary="13:00 - 16:00" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Coffee />
+            </ListItemIcon>
+            <ListItemText primary="16:00 - 19:00" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SportsTennis />
+            </ListItemIcon>
+            <ListItemText primary="19:00 - 21:00" />
+          </ListItemButton>
+        </ListItem>
+      </CustomPaper>
+    </Box>
+  );
+};
+
+const LeavePlan = () => {
+  return (
+    <CustomPaper variant="elevation" elevation={3}>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <WifiOff />
+          </ListItemIcon>
+          <ListItemText primary="Saturday, 26 January 2025" />
+        </ListItemButton>
+      </ListItem>
+      <Divider />
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <BeachAccess />
+          </ListItemIcon>
+          <ListItemText primary="Sunday, 27 January 2025" />
+        </ListItemButton>
+      </ListItem>
+    </CustomPaper>
   );
 };
 
@@ -83,56 +137,17 @@ export default () => {
         Working Hours
       </Typography>
       <Box textAlign={"center"} margin={2}>
-        <Typography variant="subtitle1" align="left">
-          Days
-        </Typography>
-        <CustomPaper variant="elevation" elevation={3}>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <DateRange />
-              </ListItemIcon>
-              <ListItemText primary="Saturday - Sunday" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <DateRange />
-              </ListItemIcon>
-              <ListItemText primary="Monday, Wednesday, Thursday" />
-            </ListItemButton>
-          </ListItem>
-        </CustomPaper>
+        <ByDay />
         <br />
         <Typography variant="subtitle1" align="left">
           Hours
         </Typography>
-        <CustomPaper variant="elevation" elevation={3}>
-          <Hourly />
-        </CustomPaper>
+        <ByHour />
         <br />
         <Typography variant="subtitle1" align="left">
           Leave Plan
         </Typography>
-        <CustomPaper variant="elevation" elevation={3}>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <WifiOff />
-              </ListItemIcon>
-              <ListItemText primary="Saturday, 26 January 2025" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <BeachAccess />
-              </ListItemIcon>
-              <ListItemText primary="Sunday, 27 January 2025" />
-            </ListItemButton>
-          </ListItem>
-        </CustomPaper>
+        <LeavePlan />
       </Box>
     </div>
   );
