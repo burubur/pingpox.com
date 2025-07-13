f44339179b6cbb3bbfcc47919d95efac2a4e1bfb
Burhan Mubarok <burhanmubarok@gmail.com>
2025-07-13 18:25:41 +0700

    Install vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
